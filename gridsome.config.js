c65396ccdb2d09be32d356ff08c0ba3e619173f7
samuelgoddard <hello@samgoddard.co.uk>
2019-07-23 17:49:30 +0100

    added tailwind plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,6 +15,8 @@ module.exports = {
         apiUrl: 'https://site-api.datocms.com',
         typeName: 'DatoCms'
       }
+    },{
+      use: 'gridsome-plugin-tailwindcss'
     }
   ]
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -62,9 +62,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
